01d8a34e9d70b21561a0ed6c302d7b8313ffe2fc
kahjyun <kahjyun@hotmail.com>
2023-01-04 23:04:50 +0800

    initialcommit

--- contact.php
+++ contact.php
@@ -11,32 +11,34 @@
 <body>
 
 <?php //storing name, email, and message
-$name = $_POST["name"];
-$visitor_email = $_POST["email"];
-$message = $_POST["subject"];
+if (!empty($_POST)) {
+    $name = $_POST["name"];
+    $visitor_email = $_POST["email"];
+    $message = $_POST["subject"];
+    $to = 'kahjyun@hotmail.com';
+    $email_from = 'fastumkj@hotmail.com';
 
-$email_from = 'fastumkj@hotmail.com';
+    $email_subject = 'Message from your website!';
 
-$email_subject = 'Message from your website!';
+    /*$email_body = "User Name: $name.\n".
+    "User Email: $visitor_email .\n"
+    "User Message: $message .\n";*/
 
-/*$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email .\n"
-                    "User Message: $message .\n";*/
+    $email_body = "User Name:" . $name .
+        "\n User Email:" . $visitor_email .
+        "\n User Message:" . $message . "\n";
 
-$email_body = "User Name:".$name.
-"\n User Email:".$visitor_email.
-"\n User Message:".$message."\n";
-                    
-$to = 'kahjyun@hotmail.com';
 
-$headers = "From: $email_from /r\n";
-$headers .= "MIME-Version: 1.0" . "\r\n"; $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "From: $email_from /r\n";
+    $headers .= "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-$headers = "Reply To: $visitor_email \r\n";
+    $headers = "Reply To: $visitor_email \r\n";
 
-mail($to, $email_subject, $email_body, $headers);
+    mail($to, $email_subject, $email_body, $headers);
 
-header("Location: contact.html");
+    header("Location: contact.html");
+}
 ?>
 
 <div class="mycontact">
